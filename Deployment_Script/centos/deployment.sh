ca5cce70f5ddecf7d9e8c9e89def9cb05577177d
gandhark <gandhark@cybage.com>
2015-08-24 19:21:04 +0530

    ef

--- Deployment_Script/centos/deployment.sh
+++ Deployment_Script/centos/deployment.sh
@@ -11,12 +11,13 @@ echo "###########################################################"
 	git clone http://$USER_REPO:$PASS_REPO@$GIT_URL $CONFFILENAME
 	ls-lrt;
 	#unzip   $ZIPNAME.zip -d $PROJECTDIR
-	chmod -R 777 $CONFFILENAME/app/storage
+	chmod -R 777 /opt/$CONFFILENAME/app/storage
 	#rm -rf $ZIPNAME.zip  
 
 echo "Giving  permission to clone repo source code"
 pwd
 chmod -R 755 /opt/$CONFFILENAME
+chmod -R 777 /opt/$CONFFILENAME/app/storage
 echo "successfully copied into /opt..next will be to create symlink"
 #echo "Check your site on  url $SERVERNAME";
 
